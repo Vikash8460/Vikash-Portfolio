f993f6239112aedbaa600909487a554eaf3ee44e
Vikash Kumar <94213875+Vikash8460@users.noreply.github.com>
2024-03-12 18:07:22 +0530

    write the code in extra-ciricular

--- src/app/profiler/extra-ciricular/extra-ciricular/extra-ciricular.component.ts
+++ src/app/profiler/extra-ciricular/extra-ciricular/extra-ciricular.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProfilerService } from '../../profiler.service';
 
 @Component({
   selector: 'app-extra-ciricular',
   templateUrl: './extra-ciricular.component.html',
   styleUrl: './extra-ciricular.component.scss'
 })
-export class ExtraCiricularComponent {
+export class ExtraCiricularComponent implements OnInit {
+
+  extras: any
+  constructor(private profilerService: ProfilerService) { }
+  ngOnInit() {
+    this.extras = this.profilerService.extraCircular()
+  }
 
 }
