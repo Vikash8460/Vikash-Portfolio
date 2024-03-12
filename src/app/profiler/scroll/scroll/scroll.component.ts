c19d3aae4c384df70a5b4154822af8f86cc1144d
Vikash Kumar <94213875+Vikash8460@users.noreply.github.com>
2024-03-12 16:04:36 +0530

    create scroll component

--- src/app/profiler/scroll/scroll/scroll.component.ts
+++ src/app/profiler/scroll/scroll/scroll.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-scroll',
   templateUrl: './scroll.component.html',
   styleUrl: './scroll.component.scss'
 })
-export class ScrollComponent {
+export class ScrollComponent implements OnInit {
 
+  windowScrolled!: boolean;
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+      if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+          this.windowScrolled = true;
+      } 
+     else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+          this.windowScrolled = false;
+      }
+  }
+  scrollToTop() {
+      (function smoothscroll() {
+          var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+          if (currentScroll > 0) {
+              window.requestAnimationFrame(smoothscroll);
+              window.scrollTo(0, currentScroll - (currentScroll / 8));
+          }
+      })();
+  }
+  ngOnInit() {}
 }
+
+
