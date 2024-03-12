86f867dacc164b67602c9f9709a9357a4ac5d21a
Vikash Kumar <94213875+Vikash8460@users.noreply.github.com>
2024-03-12 13:21:02 +0530

    create profiler and header component

--- src/app/profiler/profiler.module.ts
+++ src/app/profiler/profiler.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header/header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
     CommonModule
   ]
