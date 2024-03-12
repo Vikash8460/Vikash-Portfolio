52348fc5f5a6426e2c7f0aa72d709d11e6c51680
Vikash Kumar <94213875+Vikash8460@users.noreply.github.com>
2024-03-12 14:17:12 +0530

    changing proflrer routing, module and component

--- src/app/profiler/profiler-routing.module.ts
+++ src/app/profiler/profiler-routing.module.ts
@@ -3,27 +3,36 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProfilerComponent } from './profiler.component';
 import { HeaderComponent } from './header/header/header.component';
 import { IntroComponent } from './intro/intro/intro.component';
+import { FooterComponent } from './footer/footer/footer.component';
+import { ExtraCiricularComponent } from './extra-ciricular/extra-ciricular/extra-ciricular.component';
+import { SkillsComponent } from './skills/skills/skills.component';
+import { ReferenceComponent } from './reference/reference/reference.component';
+import { ProjectsComponent } from './projects/projects/projects.component';
+import { ExperienceComponent } from './experience/experience/experience.component';
+import { EducationComponent } from './education/education/education.component';
+import { ContactComponent } from './contact/contact/contact.component';
+import { AboutComponent } from './about/about/about.component';
 
 const routes: Routes = [
-  { path: '', component: ProfilerComponent },
-//   { path: 'dashboard', component: DashboardComponent },
-//   { path: 'login', component: LoginComponent },
-//   { path: 'about', component: AboutComponent },
-//   { path: 'contact', component: ContactComponent },
-//   { path: 'education', component: EducationComponent },
-//   { path: 'experience', component: ExperienceComponent },
-//   { path: 'extra', component: ExtraCiricularComponent },
-//   { path: 'footer', component: FooterComponent },
-  { path: 'header', component: HeaderComponent },
-  { path: 'intro', component: IntroComponent },
-//   { path: 'portfolio', component: PortfolioComponent },
-//   { path: 'projects', component: ProjectsComponent },
-//   { path: 'reference', component: ReferenceComponent },
-//   { path: 'skills', component: SkillsComponent }
+    { path: '', component: ProfilerComponent },
+    //   { path: 'dashboard', component: DashboardComponent },
+    //   { path: 'login', component: LoginComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'education', component: EducationComponent },
+    { path: 'experience', component: ExperienceComponent },
+    { path: 'extra', component: ExtraCiricularComponent },
+    { path: 'footer', component: FooterComponent },
+    { path: 'header', component: HeaderComponent },
+    { path: 'intro', component: IntroComponent },
+    //   { path: 'portfolio', component: PortfolioComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'reference', component: ReferenceComponent },
+    { path: 'skills', component: SkillsComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class ProfilerRoutingModule { }
--- src/app/profiler/profiler.module.ts
+++ src/app/profiler/profiler.module.ts
@@ -6,6 +6,13 @@ import { ProfilerComponent } from './profiler.component';
 import { ProfilerRoutingModule } from './profiler-routing.module';
 import { FooterComponent } from './footer/footer/footer.component';
 import { AboutComponent } from './about/about/about.component';
+import { ExperienceComponent } from './experience/experience/experience.component';
+import { ExtraCiricularComponent } from './extra-ciricular/extra-ciricular/extra-ciricular.component';
+import { ProjectsComponent } from './projects/projects/projects.component';
+import { SkillsComponent } from './skills/skills/skills.component';
+import { EducationComponent } from './education/education/education.component';
+import { ReferenceComponent } from './reference/reference/reference.component';
+import { ContactComponent } from './contact/contact/contact.component';
 
 
 
@@ -15,7 +22,14 @@ import { AboutComponent } from './about/about/about.component';
     HeaderComponent,
     IntroComponent,
     FooterComponent,
-    AboutComponent
+    AboutComponent,
+    ExperienceComponent,
+    ExtraCiricularComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    EducationComponent,
+    ReferenceComponent,
+    ContactComponent
   ],
   imports: [
     CommonModule,
