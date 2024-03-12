3e35c1b1fcd0c3ab9ea9c6b8395532ff07ea7db2
Vikash Kumar <94213875+Vikash8460@users.noreply.github.com>
2024-03-12 14:13:39 +0530

    create ExtraCiricularComponent

---
+++ src/app/profiler/extra-ciricular/extra-ciricular/extra-ciricular.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-extra-ciricular',
+  templateUrl: './extra-ciricular.component.html',
+  styleUrl: './extra-ciricular.component.scss'
+})
+export class ExtraCiricularComponent {
+
+}
