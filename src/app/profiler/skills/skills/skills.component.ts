36a1816b7a5ca4cb92447637ba9016eebb0e7625
Vikash Kumar <94213875+Vikash8460@users.noreply.github.com>
2024-03-12 18:09:12 +0530

    write the code in skills component

--- src/app/profiler/skills/skills/skills.component.ts
+++ src/app/profiler/skills/skills/skills.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProfilerService } from '../../profiler.service';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
-export class SkillsComponent {
+export class SkillsComponent implements OnInit {
+  skills: any
+
+  constructor(private profilerService:ProfilerService) { }
+
+    ngOnInit() {
+
+      this.skills =  this.profilerService.skills()
+    }
+
 
 }
+
