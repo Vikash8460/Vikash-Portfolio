705b6cb0d788f65012b36c9927e752eca5ee977d
Vikash Kumar <94213875+Vikash8460@users.noreply.github.com>
2024-03-12 18:08:19 +0530

    write the code into project component

--- src/app/profiler/projects/projects/projects.component.ts
+++ src/app/profiler/projects/projects/projects.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProfilerService } from '../../profiler.service';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
 
+  config: any;
+  projects: any =[];
+
+  constructor(private profilerService:ProfilerService) { }
+
+  ngOnInit() {
+    this.projects = this.profilerService.getProjects()
+    console.log(this.projects)
+    this.config = {
+      itemsPerPage: 6,
+      currentPage: 1,
+      totalItems: this.projects.length
+    };
+  }
+
+  pageChanged(event: any){
+    this.config.currentPage = event;
+  }
 }
+
+
