d2841ae35b104cf73caece6a2a17416605303045
Vikash Kumar <94213875+Vikash8460@users.noreply.github.com>
2024-03-12 16:42:47 +0530

    write the code about .html,ts,scss

--- src/app/profiler/about/about/about.component.ts
+++ src/app/profiler/about/about/about.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProfilerService } from '../../profiler.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  
+  about1: any
+  about2: any  
+ 
+
+ constructor(private profilerService:ProfilerService) { }
+
+  ngOnInit() {
+    this.about1 =  this.profilerService.about
+    this.about2 =  this.profilerService.about2
+  }
 
 }
